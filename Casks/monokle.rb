cbfd32373aec231d5286877c3525ba688757f644
Zeynep Köse <zeynepkose@Zeyneps-MacBook-Pro.local>
2022-09-06 23:24:23 +0300

    add monakle

--- Casks/monokle.rb
+++ Casks/monokle.rb
@@ -1,10 +1,9 @@
 cask "monokle" do  
     version "1.10.3"
-    url "https://github.com/kubeshop/monokle/releases/download/test/Monokle-mac-universal.tar.gz"
-    sha256 "fab97eabcab3fac5a361036801b7c32b6fda9906bff1be78c4dd9c8561899447"
+    url "https://github.com/kubeshop/monokle/releases/download/latest-version/Monokle-mac-universal.dmg"
+    sha256 "8505c527cd2d03d4eded980d68d83efe997f2afda050f7c0817c41f5b8e2859e  dist/Monokle-mac-universal.dmg"
     name "Monokle"
     desc "UI for managing k8s manifests"
     homepage "https://monokle.io/"
-
     app "Monokle.app"
   end
