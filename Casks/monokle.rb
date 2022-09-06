957d06a902ba5f2dd4d2233751ee73583aea8aee
Zeynep Köse <zeynepkose@Zeyneps-MacBook-Pro.local>
2022-09-06 23:30:14 +0300

    change sha256

--- Casks/monokle.rb
+++ Casks/monokle.rb
@@ -1,7 +1,7 @@
 cask "monokle" do  
     version "1.11.0"
     url "https://github.com/kubeshop/monokle/releases/download/latest-version/Monokle-mac-universal.dmg"
-    sha256 "8505c527cd2d03d4eded980d68d83efe997f2afda050f7c0817c41f5b8e2859e"
+    sha256 "b5e59dad38a160bebaef845ba724ff30703d662e57fb7dd13ffdfb2b2a63f1eb"
     name "Monokle"
     desc "UI for managing k8s manifests"
     homepage "https://monokle.io/"
