a05d95ed6c9f8aeae2d635d70661568ab09dee20
Erdi Köse <me@erdkse.com>
2022-09-30 13:20:43 +0300

    chore: new release

--- Casks/monokle.rb
+++ Casks/monokle.rb
@@ -1,7 +1,7 @@
 cask "monokle" do  
     version "1.10.3"
-    url "https://github.com/kubeshop/monokle/releases/download/test/Monokle-mac-universal.dmg"
-    sha256 "9e86fde6a64271424867d707f9fd3f2a7523faca9754f50d1b6c3831cc232c8a"
+    url "https://github.com/kubeshop/monokle/releases/download//Monokle-mac-universal.dmg"
+    sha256 "ebc3497bd3c82bfb3a70c249eadf8f5465b5b314e1ae5dab5ebd360351dacb9c"
     name "Monokle"
     desc "UI for managing k8s manifests"
     homepage "https://monokle.io/"
